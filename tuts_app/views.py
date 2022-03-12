4380dabac5d622d6cf5ad5eeb15e98104cf61e14
bibek977 <bhattaraibibek91@gmail.com>
2022-03-12 17:01:30 +0545

    success message updated

--- tuts_app/views.py
+++ tuts_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from tuts_app.models import Candle
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 def index(request):
     if request.user.is_authenticated:
@@ -18,6 +19,9 @@ def contact(request):
         desc = request.POST.get('desc')
         candle = Candle(name = name, desc=desc)
         candle.save()
+
+        messages.success(request, f'{name} details updated.')
+
     return render(request, "contact.html")
 
 def loginuser(request):
